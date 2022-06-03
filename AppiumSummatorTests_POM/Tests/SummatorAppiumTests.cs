e30c58f5aa41b66b201bd8b1f6ffe7df48c92ee8
milentasev <48089006+milentasev@users.noreply.github.com>
2022-06-03 17:05:11 +0300

    Initial Commit

---
+++ AppiumSummatorTests_POM/Tests/SummatorAppiumTests.cs
@@ -0,0 +1,57 @@
+﻿using AppiumSummatorTests_POM.Window;
+using NUnit.Framework;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Enums;
+using OpenQA.Selenium.Appium.Service;
+using OpenQA.Selenium.Appium.Windows;
+using System;
+
+namespace AppiumSummatorTests_POM.Tests
+{
+	public class SummatorAppiumTests
+	{
+		private WindowsDriver<WindowsElement> driver;
+		private AppiumOptions options;
+		private AppiumLocalService appiumLocal;
+
+		[OneTimeSetUp]
+		public void OpenApp()
+		{
+			this.options = new AppiumOptions() {PlatformName = "Windows" };
+			options.AddAdditionalCapability(MobileCapabilityType.App, @"ENTER THE FULL PATH OF SummatorDesktopApp.exe");
+			this.appiumLocal = new AppiumServiceBuilder().UsingAnyFreePort().Build();
+			appiumLocal.Start();
+
+			this.driver = new WindowsDriver<WindowsElement>(appiumLocal, options);
+		}
+
+		[OneTimeTearDown]
+		public void ShutDownApp()
+		{
+			this.driver.CloseApp();
+			driver.Quit();
+			this.appiumLocal.Dispose();
+		}
+
+		[TestCase("0", "0", "0")]
+		[TestCase("5", "5", "10")]
+		[TestCase("-5", "-5", "-10")]
+		[TestCase("5", "-5", "0")]
+		[TestCase("-5", "5", "0")]
+		[TestCase("hello", "5", "error")]
+		[TestCase("5", "hello", "error")]
+		[TestCase("hello", "hello", "error")]
+		[TestCase("", "5", "error")]
+		[TestCase("5", "", "error")]
+		[TestCase("", "", "error")]
+		public void Summator_Test(string firstNum, string secondNum, string expectedResult)
+		{
+			var window = new SummatorWindow(driver);
+			var actualResult = window.Calculate(firstNum, secondNum);
+
+			Assert.That(actualResult, Is.EqualTo(expectedResult));
+			
+		}
+	}
+}
+
---
+++ AppiumSummatorTests_POM/Window/SummatorWindow.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium.Appium.Windows;
+
+namespace AppiumSummatorTests_POM.Window
+{
+	public class SummatorWindow
+	{
+		private readonly WindowsDriver<WindowsElement> driver;
+
+		public SummatorWindow (WindowsDriver<WindowsElement> driver)
+		{
+			this.driver = driver;
+		}
+
+		public WindowsElement firstNumField => driver.FindElementByAccessibilityId("textBoxFirstNum");
+		public WindowsElement secondNumField => driver.FindElementByAccessibilityId("textBoxSecondNum");
+		public WindowsElement resultField => driver.FindElementByAccessibilityId("textBoxSum");
+		public WindowsElement calculateButton => driver.FindElementByAccessibilityId("buttonCalc");
+
+		public string Calculate(string field1, string field2)
+		{
+			firstNumField.Clear();
+			firstNumField.SendKeys(field1);
+
+			secondNumField.Clear();
+			secondNumField.SendKeys(field2);
+
+			calculateButton.Click();
+
+			return resultField.Text;
+		}
+	}
+}
+
